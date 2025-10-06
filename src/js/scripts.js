2b7c6192e76bf2c4cc7febeebd2efb31ba1fc871
LishunSLD <seanlarrydriesen@gmail.com>
2025-10-06 11:23:52 -0400

    5e carte

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,52 +3,52 @@ const back1 = document.querySelector("#carte1").querySelector(".back")
 carte1.addEventListener("click", () => {
     carte1.classList.toggle("d-none")
     back1.classList.toggle("d-none")
-  })
+})
 back1.addEventListener("click", () => {
     carte1.classList.toggle("d-none")
     back1.classList.toggle("d-none")
-  })
+})
 
 const carte2 = document.querySelector("#carte2").querySelector(".carte")
 const back2 = document.querySelector("#carte2").querySelector(".back")
 carte2.addEventListener("click", () => {
     carte2.classList.toggle("d-none")
     back2.classList.toggle("d-none")
-  })
+})
 back2.addEventListener("click", () => {
     carte2.classList.toggle("d-none")
     back2.classList.toggle("d-none")
-  })
+})
 
 const carte3 = document.querySelector("#carte3").querySelector(".carte")
 const back3 = document.querySelector("#carte3").querySelector(".back")
 carte3.addEventListener("click", () => {
     carte3.classList.toggle("d-none")
     back3.classList.toggle("d-none")
-  })
+})
 back3.addEventListener("click", () => {
     carte3.classList.toggle("d-none")
     back3.classList.toggle("d-none")
-  })
+})
 
 const carte4 = document.querySelector("#carte4").querySelector(".carte")
 const back4 = document.querySelector("#carte4").querySelector(".back")
 carte4.addEventListener("click", () => {
     carte4.classList.toggle("d-none")
     back4.classList.toggle("d-none")
-  })
+})
 back4.addEventListener("click", () => {
     carte4.classList.toggle("d-none")
     back4.classList.toggle("d-none")
-  })
+})
 
 const carte5 = document.querySelector("#carte5").querySelector(".carte")
 const back5 = document.querySelector("#carte5").querySelector(".back")
 carte5.addEventListener("click", () => {
     carte5.classList.toggle("d-none")
     back5.classList.toggle("d-none")
-  })
+})
 back5.addEventListener("click", () => {
     carte5.classList.toggle("d-none")
     back5.classList.toggle("d-none")
-  })
+})
